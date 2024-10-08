c3a3cdddfe78aa5a548a0a76fa32549f436e29d5
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-10-09 00:21:18 +0530

    add

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var ctx = context.Background()
 var client *redis.Client
 
 func main() {
-    err := godotenv.Load()
+    err := godotenv.Load(".env")
     if err != nil {
         log.Fatalf("Error loading .env file: %v", err)
     }
